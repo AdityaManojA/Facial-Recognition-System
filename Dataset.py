a14c0a3320856ebd7ffd6fc64e8557c1ac59f8e4
AdityaManojA <130238815+AdityaManojA@users.noreply.github.com>
2024-12-01 22:22:31 +0530

    attendance

--- Dataset.py
+++ Dataset.py
@@ -1,16 +1,56 @@
-# This is a sample Python script.
+import cv2
+import pickle
+import numpy as np
+import os
+video=cv2.VideoCapture(0)
+facedetect=cv2.CascadeClassifier('haarcascade_frontalface_default .xml')
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+faces_data=[]
+
+i=0
+
+name=input("Enter Your Name: ")
+
+while True:
+    ret,frame=video.read()
+    gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces=facedetect.detectMultiScale(gray, 1.3 ,5)
+    for (x,y,w,h) in faces:
+        crop_img=frame[y:y+h, x:x+w, :]
+        resized_img=cv2.resize(crop_img, (50,50))
+        if len(faces_data)<=100 and i%10==0:
+            faces_data.append(resized_img)
+        i=i+1
+        cv2.putText(frame, str(len(faces_data)), (50,50), cv2.FONT_HERSHEY_COMPLEX, 1, (50,50,255), 1)
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (50,50,255), 1)
+    cv2.imshow("Frame",frame)
+    k=cv2.waitKey(1)
+    if len(faces_data)==50:
+        break
+video.release()
+cv2.destroyAllWindows()
+
+faces_data=np.asarray(faces_data)
+faces_data=faces_data.reshape(100, -1)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+if 'names.pkl' not in os.listdir('data/'):
+    names=[name]*100
+    with open('data/names.pkl', 'wb') as f:
+        pickle.dump(names, f)
+else:
+    with open('data/names.pkl', 'rb') as f:
+        names=pickle.load(f)
+    names=names+[name]*100
+    with open('data/names.pkl', 'wb') as f:
+        pickle.dump(names, f)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if 'faces_data.pkl' not in os.listdir('data/'):
+    with open('data/faces_data.pkl', 'wb') as f:
+        pickle.dump(faces_data, f)
+else:
+    with open('data/faces_data.pkl', 'rb') as f:
+        faces=pickle.load(f)
+    faces=np.append(faces, faces_data, axis=0)
+    with open('data/faces_data.pkl', 'wb') as f:
+        pickle.dump(faces, f)
